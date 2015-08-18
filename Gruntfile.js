448ed285487451aa00c2a4c7c299b2c56d8dac04
afonsopacifer <afonsopolis@gmail.com>
2015-08-18 17:48:50 -0300

    start

---
+++ Gruntfile.js
@@ -0,0 +1,103 @@
+module.exports = function( grunt ) {
+
+  "use strict"
+
+  grunt.initConfig({
+
+    pkg: grunt.file.readJSON('package.json'),
+
+    //watch and compile all files
+    watch: {
+
+      html: {
+        files: 'prod/*.html',
+      },
+
+      css: {
+        files: 'prod/assets/styles/*.css',
+      },
+
+      jd: {
+        files: ['dev/*.jade','dev/**/*.jade'],
+        tasks: ['jade:compile'],
+      },
+
+      styl: {
+        files: ['dev/assets/styles/*.styl','dev/assets/**/*.styl'],
+        tasks: ['stylus:compile'],
+      },
+
+      javascript: {
+        files: 'dev/assets/scripts/*.js',
+        tasks: ['uglify'],
+      },
+
+      options: {
+        livereload: true
+      }
+
+    },
+
+    // The jade compile task
+    jade: {
+      //compile for production
+      compile: {
+        files: {
+					"prod/index.html": ["dev/index.jade"]
+				}
+      }
+    },
+
+    //The stylus compile task
+    stylus: {
+      //compile for production
+      compile: {
+        files: {
+          'prod/assets/styles/style.min.css':'dev/assets/styles/style.styl' // 1:1 compile
+        }
+      }
+    },
+
+    //concat all JS
+    concat: {
+			dist: {
+		      src: 'dev/assets/scripts/*.js',
+		      dest: 'prod/assets/scripts/scripts.js'
+		    }
+		},
+
+    //compress all js
+    uglify: {
+      my_target: {
+        files: {
+        'prod/assets/scripts/scripts.min.js': ['dev/assets/scripts/*.js']
+        }
+      }
+    },
+
+    //Starts the static server
+    connect: {
+      server: {
+        options: {
+          port: 9000,
+          base: "prod/",
+          hostname: "localhost",
+          livereload: true,
+          open: true
+        }
+      }
+
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-jade');
+  grunt.loadNpmTasks('grunt-contrib-stylus');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.registerTask( 'dev', ['connect','watch']);
+
+};
---
+++ README.md
@@ -0,0 +1,45 @@
+# Busca Starter Kit
+
+> Uma base para iniciar os projetos.
+
+**Dica 1:** O projeto usa o [flex-grid-framework](https://github.com/afonsopacifer/flex-grid-framework) como sistema de grid. De uma olhada na [documentação](https://afonsopacifer.github.io/flex-grid-framework/) para saber mais.
+
+**Dica 3:** Use os [snippets](snippets.md) para agilizar o desenvolvimento.
+
+**Dica 3:** Para manter a organização, [siga o guia de estilos de código](code-guide.md).
+
+### Como usar?
+
+**Siga os passos abaixo :)**
+
+#### 1 - Clone o repositório
+
+    $ git clone https://github.com/busca-dev/busca-starter-kit.git
+
+#### 2 - Vá até a pasta e instale todas as dependências
+
+	$ cd nome-do-projeto/
+  $ npm install
+
+#### 3 - Inicio o ambiente de senseolvolvimento com o Grunt
+
+**Inicia um servidor estático na porta 9000, escuta e compila todos os arquivos .jade/.styl, otimiza todas as imagens images da pasta img, ativa o livereload e concatena e minifica todos os arquivos .js**
+
+	$ grunt dev
+
+### Lista de tarefas do Grunt
+
+- ?????
+
+### Como fazer o deploy?
+
+    $ ?????
+
+### Como manter o projeto versionado corretamente
+
+    $ ?????
+
+### Estrutura das pastas
+
+<pre>
+</pre>
